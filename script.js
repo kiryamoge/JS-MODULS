e4dd35236f493e05024c301ac5dc39869a20abbd
kiryamoge <139700463+kiryamoge@users.noreply.github.com>
2023-11-07 20:08:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+class Button {
+    constructor(id, label, user){
+      this.id = id;
+      this.label = label;
+      this.user = user;
+    }
+  
+    onClick(){
+      console.log(this.user.name);
+    }
+  
+    render(){
+      const button = document.createElement('button');
+      button.id = this.id;
+      button.textContent = this.label;
+      button.addEventListener('click', this.onClick.bind(this));
+      return button;
+    }
+  }
+  
+  const data = [
+    {id: 1, label: "Button1", user: {name: "Pit"}},
+    {id: 2, label: "Button2", user: {name: "Jane"}},
+    {id: 3, label: "Button3", user: {name: "Kyle"}}
+  ];
+  
+  const container = document.getElementById("app");
+  
+  data.forEach(item =>{
+    const button = new Button(item.id, item.label, item.user);
+    container.appendChild(button.render());
+  });
